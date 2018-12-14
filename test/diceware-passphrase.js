4f84a2963892dd4dc3792d22f647aaa87adb646b
bredele <olivier.wietrich@gmail.com>
2018-12-14 12:33:54 -0700

    Generate a 7 words passphrase

--- test/diceware-passphrase.js
+++ test/diceware-passphrase.js
@@ -9,6 +9,7 @@ const list = Object.values(require('diceware-list/list.json'))
 test('should generate a 7 words passphrase', assert => {
   assert.plan(8)
   const words = passphrase()
+  console.log(words)
   assert.equal(words.length, 7)
   words.map(word => {
     assert.equal(list.indexOf(word) > -1, true)
